bb9da1f53ce6b7df07d0e2854eeff99e5beaf9e5
GustavoVieck <gustavo.dores@sptech.school>
2026-04-13 15:47:19 -0300

    Retirando algumas implementações do jwt

--- blsd-api-bend/src/main/java/com/blessed/blsd_api_bend/dto/cliente/ClienteRequestDTO.java
+++ blsd-api-bend/src/main/java/com/blessed/blsd_api_bend/dto/cliente/ClienteRequestDTO.java
@@ -1,5 +1,6 @@
 package com.blessed.blsd_api_bend.dto.cliente;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -17,6 +18,7 @@ public class ClienteRequestDTO {
     @NotBlank
     private String nome;
     @NotBlank
+    @Email
     private String email;
     @NotBlank
     private String senha;
@@ -27,12 +29,7 @@ public class ClienteRequestDTO {
     private LocalDate dataNasc;
     @NotBlank
     private String telefone;
-    @NotNull
-    private Long acessoId;
-    @NotNull
-    private Long enderecoId;
 
-    private Long consultaId;
 
 
 
--- blsd-api-bend/src/main/java/com/blessed/blsd_api_bend/dto/cliente/ClienteResponseDTO.java
+++ blsd-api-bend/src/main/java/com/blessed/blsd_api_bend/dto/cliente/ClienteResponseDTO.java
@@ -1,4 +1,14 @@
 package com.blessed.blsd_api_bend.dto.cliente;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
 public class ClienteResponseDTO {
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDate dataNasc;
+    private String telefone;
 }
